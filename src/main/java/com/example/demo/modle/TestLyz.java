ba75b882c7e6459953cb35742b1cb3aca4c4f772
liyongzhen <liyongzhen_it@163.com>
2021-03-14 18:51:34 +0800

    提交工程

---
+++ src/main/java/com/example/demo/modle/TestLyz.java
@@ -0,0 +1,10 @@
+package com.example.demo.modle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestLyz {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+    }
+}
---
+++ src/main/java/com/example/demo/modle/user.java
@@ -0,0 +1,14 @@
+package com.example.demo.modle;
+
+public class user {
+    private int age=20;
+    private String name ="tt";
+
+    @Override
+    public String toString() {
+        return "user{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
